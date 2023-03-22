99fc13092de312eb8a8fddf42e44df168d7680f4
sajith <sajith@techis.io>
2023-03-23 00:32:03 +0530

    updated

---
+++ config/constants.py
@@ -0,0 +1,85 @@
+
+DATE_FORMAT = '%Y-%m-%d'
+DATE_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+USER_STATUS = (
+    ('active', 'Active'),
+    ('deleted', 'Deleted')
+)
+
+USER_ROLE = (
+    ('director', 'Director'),
+    ('manager', 'Manager'),
+    ('leader', 'Leader'),
+    ('member', 'Member'),
+    ('co-ordinator','Co-Ordinator')
+)
+
+USER_TEAM = (
+    ('director', 'Director'),
+    ('marketer', 'Marketer'),
+    ('engineer', 'Engineer'),
+    ('hr', 'HR'),
+    ('sales', 'Sales'),
+    ('admin', 'Admin'),
+    ('communication','Communication')
+)
+
+USER_BATCH = (
+    ('n/a', 'N/A'),
+    ('batch1', 'Batch1'),
+    ('batch2', 'Batch2'),
+    ('batch3', 'Batch3'),
+    ('batch4', 'Batch4'),
+    ('batch5', 'Batch5'),
+    ('batch6', 'Batch6'),
+    ('batch7', 'Batch7'),
+    ('batch8', 'Batch8'),
+    ('under_training', 'Under Training')
+)
+USER_SHIFT =(
+    ('n/a', 'N/A'),
+    ('day', 'Day'),
+    ('night','Night'),
+    ('afternoon','Afternoon')
+)
+
+
+LEAVE_STATUS = (
+    ('applied', 'Applied'),
+    ('req_modification', 'Request Modification'),
+    ('forwarded', 'Forwarded'),
+    ('approved', 'Approved'),
+    ('rejected','Rejected')
+)
+LEAVE_TYPE =(
+    ('n/a', 'N/A'),
+    ('casual_leave', 'Casual Leave'),
+    ('sick_leave', 'Sick Leave'),
+    ('maternity_leave', 'Maternity Leave'),
+    ('paternity_leave', 'Paternity Leave'),
+    ('loss_of_pay_leave', 'Loss of Pay'),
+    ('emergency', 'Emergency Leave')
+)
+
+LEAVE_STATUS = (
+    ('n/a', 'N/A'),
+    ('approved', 'Approved'),
+    ('rejected', 'Rejected'),
+    ('pending', 'Pending'),
+    ('cancelled', 'Cancelled')
+)
+
+APPLIED_TO = (
+    ('saravana_kumar', 'Saravana Kumar'),
+    ('rajeshwari', 'Rajeshwari'),
+    ('shiva_ganesh', 'Shiva Ganesh'),
+    ('anshu_kumar', 'Anshu Kumar'),
+    ('aishwarya', 'Aishwarya Shetty'),
+    ('sajid', 'Sajid Khurshid'),
+    ('prasanna', 'Prasanna Akash'),
+    ('jithin', 'Jithin Peter'),
+    ('nitesh', 'Nitesh Tiwari'),
+    ('thejas', 'Thejas BM')
+
+)
